45b73b3aff7c61105781218bfa492c82f4638252
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2025-06-23 17:23:33 +0300

    Update app

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,37 +1,38 @@
 import { useEffect, useState } from 'react';
-import './Character.css';
 
-function Character({ userId }) {
+function Character() {
   const [characterData, setCharacterData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [user, setUser] = useState(null);
   const API_BASE_URL = 'http://localhost:8000';
 
+  // Получение данных из Telegram
   useEffect(() => {
     const telegram = window.Telegram?.WebApp;
     if (telegram) {
       telegram.ready();
       const initData = telegram.initDataUnsafe;
-      const userId = initData?.user?.id;
-
-      setUser({
-        id: userId,
-        firstName: initData?.user?.first_name,
-        lastName: initData?.user?.last_name,
-      });
+      if (initData?.user?.id) {
+        setUser({
+          id: initData.user.id,
+          firstName: initData?.user?.first_name,
+          lastName: initData?.user?.last_name,
+        });
+      }
     }
   }, []);
 
+  // Загрузка персонажа при изменении user
   useEffect(() => {
     const fetchCharacter = async () => {
-      if (!userId) {
+      if (!user?.id) {
         setError('Пользователь не выбран');
         setLoading(false);
         return;
       }
       try {
-        const res = await fetch(`${API_BASE_URL}/api/get_character/${userId}`);
+        const res = await fetch(`${API_BASE_URL}/api/get_character/${user.id}`);
         if (res.ok) {
           const data = await res.json();
           setCharacterData(data);
@@ -47,7 +48,7 @@ function Character({ userId }) {
     };
 
     fetchCharacter();
-  }, [userId]);
+  }, [user]);
 
   if (loading) return <div>Загрузка персонажа...</div>;
   if (error) return <div style={{ color: 'red' }}>{error}</div>;
