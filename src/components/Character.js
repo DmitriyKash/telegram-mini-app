4e3b1e0c0205ee6ff120c5c37be3a0db7aefd897
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2025-06-23 21:20:49 +0300

    Update app

--- src/components/Character.js
+++ src/components/Character.js
@@ -4,7 +4,7 @@ function Character({ userId }) {
   const [characterData, setCharacterData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const API_BASE_URL = 'https://192.168.0.131:8001';
+  const API_BASE_URL = 'http://192.168.0.131:8001';
 
   // Загружаем персонажа по userId
   useEffect(() => {
